9101f876ebad98dbd96704dcc6d774badfc3559f
A. S. Vorobyov <svirex@gmail.com>
2024-02-07 22:31:16 +0400

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
 
 	"github.com/Svirex/microurl/internal/pkg/app"
 )
+
+func getHostFromAddr(addr string) (string, error) {
+	a, err := url.Parse(addr)
+	if err != nil {
+		return "", err
+	}
+	return a.Host, nil
+}
 
 func main() {
 	var host string
@@ -14,9 +23,14 @@ func main() {
 	flag.StringVar(&host, "a", "localhost:8080", "<host>:<port>")
 	flag.StringVar(&baseURL, "b", "", "base url")
 	flag.Parse()
+	host, err := getHostFromAddr(host)
+	if err != nil {
+		panic(err)
+	}
 	if baseURL == "" {
 		baseURL = host
 	}
+
 	fmt.Println(host, baseURL)
 	app.Run(host, baseURL)
 }
