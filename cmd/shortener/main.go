cd82a623fbc3512df626b17b7d3e0b1b70aa56d2
Артём Воробьёв [продвинутый разработчик Go 27 когорта] <svirex@yandex.ru>
2024-02-08 14:36:04 +0400

    remove fmt

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Svirex/microurl/internal/pkg/app"
 	"github.com/Svirex/microurl/internal/pkg/config"
 )
 
 func main() {
 	cfg := config.Parse()
-	fmt.Println(cfg.Addr, cfg.BaseURL)
 	app.Run(cfg.Addr, cfg.BaseURL)
 }
