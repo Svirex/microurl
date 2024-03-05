39d78950394be647352dc7d69216861cad9b2d40
A. S. Vorobyov <svirex@gmail.com>
2024-03-05 14:16:19 +0400

    iter9

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -9,12 +9,12 @@ import (
 	"github.com/Svirex/microurl/internal/storage"
 )
 
-const SHORT_URL_LENGTH uint = 8
+const shortURLLength uint = 8
 
 func Run(cfg *config.Config) error {
 	generator := generators.NewSimpleGenerator(time.Now().UnixNano())
 	repository := storage.NewMapRepository()
-	options := server.NewOptions(cfg.Addr, cfg.BaseURL, cfg.FileStoragePath, generator, repository, SHORT_URL_LENGTH)
+	options := server.NewOptions(cfg.Addr, cfg.BaseURL, cfg.FileStoragePath, generator, repository, shortURLLength)
 	server, err := server.NewServer(options)
 	if err != nil {
 		return err
