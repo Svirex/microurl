999d70f23e6522f6610d60917605946f23f3e479
Artyom Vorobyov <svirex@gmail.com>
2024-05-31 21:48:42 +0300

    add doc

--- internal/core/domain/domain.go
+++ internal/core/domain/domain.go
@@ -1,3 +1,4 @@
+// Пакет domain описывает основные сущности, используемые в проекте.
 package domain
 
 type ShortID string
@@ -5,17 +6,20 @@ type ShortURL string
 type URL string
 type UID string
 
+// Record определяет тип для записи к БД.
 type Record struct {
 	UID UID
 	URL URL
 }
 
+// URLData - тип записи реального URL и сокращенного URL.
 type URLData struct {
 	URL      URL     `json:"original_url"`
 	ShortID  ShortID `json:"-"`
 	ShortURL URL     `json:"short_url"`
 }
 
+// BatchRecord - тип записи при добавления записей батчей.
 type BatchRecord struct {
 	CorrID   string  `json:"correlation_id"`
 	URL      URL     `json:"original_url"`
@@ -23,6 +27,7 @@ type BatchRecord struct {
 	ShortURL URL     `json:"short_url"`
 }
 
+// BackupRecord - тип для хранения данных в файле.
 type BackupRecord struct {
 	UUID    string  `json:"uuid"`
 	ShortID ShortID `json:"short_url"`
@@ -30,6 +35,7 @@ type BackupRecord struct {
 	UID     UID     `json:"uid,omitempty"`
 }
 
+// DeleteData - данные для пометки URL как удаленного.
 type DeleteData struct {
 	UID     string
 	ShortID string
--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -10,51 +10,82 @@ import (
 var ErrAlreadyExists = errors.New("already exists")
 var ErrNotFound = errors.New("not found")
 
+// ShortenerService - интерфейс сервиса сокращения ссылок.
 type ShortenerService interface {
+	// Add - добавить запись и вернуть сокращенный URL.
 	Add(context.Context, *domain.Record) (domain.ShortURL, error)
+
+	// Get - получить URL по сокращенному ID.
 	Get(ctx context.Context, shortID domain.ShortID) (domain.URL, error)
+
+	// Batch - добавить несколько записей.
 	Batch(ctx context.Context, uid domain.UID, data []domain.BatchRecord) ([]domain.BatchRecord, error)
+
+	// UserURLs - получить все записи для определенного пользователя
 	UserURLs(ctx context.Context, uid domain.UID) ([]domain.URLData, error)
+
+	// Shutdown - завершить сервис
 	Shutdown() error
 }
 
+// DBCheckerService - интерфейс сервиса проверки соединения с БД.
 type DBCheckerService interface {
+	// DBCheckerService - проверка, что БД доступна.
 	Ping(context.Context) error
+
+	// Shutdown - выключение сервиса
 	Shutdown() error
 }
 
+// ShortenerRepository - интерфейс репозитория сервиса сокращения ссылок.
 type ShortenerRepository interface {
+	// Add - добавить короткие идентификатор и URL для пользователя в БД.
+	// Если такой URL уже есть, то вернуть соответствующий ему идентификатор.
 	Add(ctx context.Context, shortID domain.ShortID, data *domain.Record) (domain.ShortID, error)
+
+	// Get - получить оригинальный URL по идентификатору.
 	Get(ctx context.Context, shortID domain.ShortID) (domain.URL, error)
+
+	// Batch - записать в БД батч записей.
 	Batch(ctx context.Context, uid domain.UID, data []domain.BatchRecord) ([]domain.BatchRecord, error)
+
+	// UserURLs - вернуть все записи для пользователя
 	UserURLs(ctx context.Context, uid domain.UID) ([]domain.URLData, error)
+
+	// Shutdown - выключить сервис
 	Shutdown() error
 }
 
+// BackupWriter - интерфейс для сохранения записей в файл.
 type BackupWriter interface {
 	Write(ctx context.Context, record *domain.BackupRecord) error
 }
 
+// BackupReader - интерфейс для чтения записей из файла.
 type BackupReader interface {
 	Next() bool
 	Read(ctx context.Context) (*domain.BackupRecord, error)
 	Restore(ctx context.Context, repo ShortenerRepository) error
 }
 
+// StringGenerator - интерфейс генератора рандомных строк.
 type StringGenerator interface {
 	Generate(ctx context.Context, size uint) string
 }
 
+// DeleterService - интерфейс сервиса, который помечает URL удаленными.
 type DeleterService interface {
 	Process(ctx context.Context, uid string, shortIDs []string)
 	Run() error
 	Shutdown() error
 }
 
+// DeleterRepository - репозиторий, который выполняет удаление записей в БД.
 type DeleterRepository interface {
 	Delete(ctx context.Context, batch []*domain.DeleteData) error
 }
 
+// DBCheck - интерфейс проверки коннекта к БД.
 type DBCheck interface {
 	Ping(context.Context) error
 }
